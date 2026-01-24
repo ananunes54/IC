5ce4879af1e6342667e2c42998ced693ceba448f
Ana Nunes <anacpn2006@gmail.com>
2026-01-24 20:35:41 -0300

    Create basic window.

--- simulation.c
+++ simulation.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <stdbool.h>
 
 int main()
 {
-  printf("hello, world");
+  if (!SDL_Init(SDL_INIT_VIDEO))
+  {
+    return -1;
+  }
+
+  int windowWidth = 680, windowHeight = 420;
+
+  SDL_Window* window = SDL_CreateWindow("simulation", windowWidth, windowHeight, SDL_WINDOW_RESIZABLE);
+  if (window == NULL)
+  {
+    SDL_Quit();
+    return -1;
+  }
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+  if (renderer == NULL)
+  {
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return -1;
+  }
+  
+  bool continueLoop = true; 
+  SDL_Event event;
+
+  while (continueLoop)
+  {
+    SDL_PollEvent(&event);
+    if (event.type == SDL_EVENT_QUIT)
+      continueLoop = false;
+    
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
 }
