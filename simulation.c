8aea6bda258cd4f65f39f8e340acc8165b376be2
Ana Nunes <anacpn2006@gmail.com>
2026-01-25 15:53:15 -0300

    Add trajectory for a particle moving at constant velocity(x-axis).

--- simulation.c
+++ simulation.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_timer.h>
 #include <stdbool.h>
 
 int main()
@@ -10,7 +11,7 @@ int main()
     return -1;
   }
 
-  int windowWidth = 680, windowHeight = 420;
+  int windowWidth = 1080, windowHeight = 940;
 
   SDL_Window* window = SDL_CreateWindow("simulation", windowWidth, windowHeight, SDL_WINDOW_RESIZABLE);
   if (window == NULL)
@@ -26,6 +27,13 @@ int main()
     SDL_Quit();
     return -1;
   }
+  SDL_SetRenderVSync(renderer, 1);
+
+  float xVelocity = 1;
+  float relativeXCoordinate = 0, relativeTimeCoordinate = 0;
+  float timeIncrement = 0.05;
+  float realXCoordinate = relativeXCoordinate + windowWidth/2;
+  float realTimeCoordinate = relativeTimeCoordinate + windowHeight/2;
   
   bool continueLoop = true; 
   SDL_Event event;
@@ -37,9 +45,11 @@ int main()
       continueLoop = false;
     
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderClear(renderer);
+    SDL_RenderPoint(renderer, realXCoordinate, realTimeCoordinate);
     SDL_RenderPresent(renderer);
 
+    realXCoordinate += xVelocity * timeIncrement * 10;
+    realTimeCoordinate -= timeIncrement * 10;
   }
 
   SDL_DestroyRenderer(renderer);
