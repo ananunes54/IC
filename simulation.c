fe790495fbc065befd4ea096b3f3ce11423fff73
Ana Nunes <anacpn2006@gmail.com>
2026-01-26 13:48:38 -0300

    Create 'vec' structure to store time and x componentes.
    Replace float variables with new 'vec' struct.

--- simulation.c
+++ simulation.c
@@ -4,6 +4,11 @@
 #include <SDL3/SDL_timer.h>
 #include <stdbool.h>
 
+typedef struct Vec
+{
+  float tComponent, xComponent;
+} Vec;
+
 int main()
 {
   if (!SDL_Init(SDL_INIT_VIDEO))
@@ -11,9 +16,17 @@ int main()
     return -1;
   }
 
-  int windowWidth = 1080, windowHeight = 940;
+  int windowWidth = 940, windowHeight = 940, windowFlag = SDL_WINDOW_RESIZABLE;
 
-  SDL_Window* window = SDL_CreateWindow("simulation", windowWidth, windowHeight, SDL_WINDOW_RESIZABLE);
+  Vec initialVelocity = {0.0, 0.1};
+  Vec initialRelativePosition = {0.0, 0.0};
+  
+  Vec velocity = initialVelocity;
+  Vec relativePosition = initialRelativePosition;
+  Vec realPosition = {relativePosition.tComponent + windowHeight/2, relativePosition.xComponent + windowWidth/2};
+  float timeIncrement = 0.05;
+  
+  SDL_Window* window = SDL_CreateWindow("simulation", windowWidth, windowHeight, windowFlag);
   if (window == NULL)
   {
     SDL_Quit();
@@ -21,20 +34,16 @@ int main()
   }
 
   SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+  
   if (renderer == NULL)
   {
     SDL_DestroyWindow(window);
     SDL_Quit();
     return -1;
   }
+
   SDL_SetRenderVSync(renderer, 1);
 
-  float xVelocity = 1;
-  float relativeXCoordinate = 0, relativeTimeCoordinate = 0;
-  float timeIncrement = 0.05;
-  float realXCoordinate = relativeXCoordinate + windowWidth/2;
-  float realTimeCoordinate = relativeTimeCoordinate + windowHeight/2;
-  
   bool continueLoop = true; 
   SDL_Event event;
 
@@ -45,11 +54,11 @@ int main()
       continueLoop = false;
     
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderPoint(renderer, realXCoordinate, realTimeCoordinate);
+    SDL_RenderPoint(renderer, realPosition.xComponent, realPosition.tComponent);
     SDL_RenderPresent(renderer);
 
-    realXCoordinate += xVelocity * timeIncrement * 10;
-    realTimeCoordinate -= timeIncrement * 10;
+    realPosition.xComponent = realPosition.xComponent + velocity.xComponent * timeIncrement * 10;
+    realPosition.tComponent = realPosition.tComponent - timeIncrement * 10;
   }
 
   SDL_DestroyRenderer(renderer);
