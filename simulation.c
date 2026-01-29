ce65bec51f57fa03ec3f38c377d11e7cc45af3b5
Ana Nunes <anacpn2006@gmail.com>
2026-01-28 21:09:51 -0300

    Add constant acceleration trajectory.

--- simulation.c
+++ simulation.c
@@ -9,6 +9,51 @@ typedef struct Vec
   float tComponent, xComponent;
 } Vec;
 
+typedef struct M2x2
+{
+  float m00, m01, m10, m11;
+} M2x2;
+
+
+M2x2 AuxM2x2 (Vec vector1, Vec vector2, float timeIncrement);
+Vec VecM2x2Multiplication (Vec vector, M2x2 matrix);
+Vec VecScale (Vec vector, float factor);
+Vec ApplyMetricTensor (Vec vector);
+
+
+M2x2 AuxM2x2 (Vec vector1, Vec vector2, float timeIncrement)
+{
+  M2x2 auxMatrix;
+  Vec covector1 = ApplyMetricTensor(vector1);
+  Vec covector2 = ApplyMetricTensor(vector2);
+  auxMatrix.m00 = (vector1.tComponent * covector2.tComponent - vector2.tComponent * covector1.tComponent) + 1;
+  auxMatrix.m01 = (vector1.tComponent * covector2.xComponent - vector2.tComponent * covector1.xComponent) * timeIncrement;
+  auxMatrix.m10 = (vector1.xComponent * covector2.tComponent - vector2.xComponent * covector1.tComponent) * timeIncrement;
+  auxMatrix.m11 = (vector1.xComponent * covector2.xComponent - vector2.xComponent * covector1.xComponent) + 1;
+  return auxMatrix;
+}
+
+Vec VecM2x2Multiplication (Vec vector, M2x2 matrix)
+{
+  Vec auxVector;
+  auxVector.tComponent = vector.tComponent * matrix.m00 + vector.xComponent * matrix.m01;
+  auxVector.xComponent = vector.tComponent * matrix.m10 + vector.xComponent * matrix.m11;
+  return auxVector;
+}
+
+Vec VecScale (Vec vector, float factor)
+{
+  Vec auxVector = {vector.tComponent * factor, vector.xComponent * factor};
+  return auxVector;
+}
+
+Vec ApplyMetricTensor (Vec vector)
+{
+  Vec auxVector = {vector.tComponent, - (vector.xComponent)};
+  return auxVector;
+}
+
+
 int main()
 {
   if (!SDL_Init(SDL_INIT_VIDEO))
@@ -16,16 +61,21 @@ int main()
     return -1;
   }
 
-  int windowWidth = 940, windowHeight = 940, windowFlag = SDL_WINDOW_RESIZABLE;
+  int windowWidth = 2080, windowHeight = 940, windowFlag = SDL_WINDOW_RESIZABLE;
 
-  Vec initialVelocity = {0.0, 0.1};
+  Vec initialVelocity = {1.0, 0.0};
   Vec initialRelativePosition = {0.0, 0.0};
-  
+  Vec initialAcceleration = {0.0, 0.05};
+
   Vec velocity = initialVelocity;
+  Vec acceleration = initialAcceleration;
   Vec relativePosition = initialRelativePosition;
   Vec realPosition = {relativePosition.tComponent + windowHeight/2, relativePosition.xComponent + windowWidth/2};
-  float timeIncrement = 0.05;
+  float timeIncrement = 0.005;
   
+  M2x2 lambdaMatrix = AuxM2x2(acceleration, velocity, timeIncrement);
+  printf("%f %f %f %f\n", lambdaMatrix.m00, lambdaMatrix.m01, lambdaMatrix.m10, lambdaMatrix.m11);
+
   SDL_Window* window = SDL_CreateWindow("simulation", windowWidth, windowHeight, windowFlag);
   if (window == NULL)
   {
@@ -47,6 +97,9 @@ int main()
   bool continueLoop = true; 
   SDL_Event event;
 
+  float referenceLineX = windowWidth / 2;
+  float referenceLineT = windowHeight / 2;
+
   while (continueLoop)
   {
     SDL_PollEvent(&event);
@@ -55,10 +108,18 @@ int main()
     
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderPoint(renderer, realPosition.xComponent, realPosition.tComponent);
-    SDL_RenderPresent(renderer);
 
-    realPosition.xComponent = realPosition.xComponent + velocity.xComponent * timeIncrement * 10;
-    realPosition.tComponent = realPosition.tComponent - timeIncrement * 10;
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderPoint(renderer, referenceLineX, referenceLineT);
+    referenceLineX += timeIncrement * 5;
+    referenceLineT -= timeIncrement * 5;
+
+    SDL_RenderPresent(renderer);
+    velocity = VecM2x2Multiplication(velocity, lambdaMatrix);
+    acceleration = VecM2x2Multiplication(acceleration, lambdaMatrix);
+    lambdaMatrix = AuxM2x2(acceleration, velocity, timeIncrement);
+    realPosition.xComponent = realPosition.xComponent + velocity.xComponent * timeIncrement * 15;
+    realPosition.tComponent = realPosition.tComponent - velocity.tComponent * timeIncrement * 15;
   }
 
   SDL_DestroyRenderer(renderer);
