f2ae568134704380f3960f31d341b666c4c304e1
Ana Nunes <anacpn2006@gmail.com>
2026-02-11 21:27:07 -0300

    add omega matrix

---
+++ include/matrix_exponential.h
@@ -0,0 +1,11 @@
+#include <complex.h>
+/*
+  Complex functions.
+*/
+double complex *c8mat_expm1 ( int n, double complex a[] );
+/*
+  Real functions.
+*/
+double *r8mat_expm1 ( int n, double a[] );
+double *r8mat_expm2 ( int n, double a[] );
+double *r8mat_expm3 ( int n, double a[] );
--- simulation.c
+++ simulation.c
@@ -1,157 +1,167 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_timer.h>
 #include <stdbool.h>
+#include <matrix_exponential.h>
 #include <math.h>
 
 typedef struct Vec
 {
-  float tComponent, xComponent;
+	float tComponent, xComponent, yComponent;
 } Vec;
 
-typedef struct M2x2
-{
-  float m00, m01, m10, m11;
-} M2x2;
 
 
-M2x2 AuxM2x2 (Vec vector1, Vec vector2, float timeIncrement);
-Vec VecM2x2Multiplication (Vec vector, M2x2 matrix);
+Vec M3x3vecMultiplication (double matrix[], Vec vector);
 Vec VecScale (Vec vector, float factor);
 Vec ApplyMetricTensor (Vec vector);
+Vec VecSum2(Vec vector1, Vec vector2);
+Vec VecSum3(Vec vector1, Vec vector2, Vec vector3);
 
 
-M2x2 AuxM2x2 (Vec vector1, Vec vector2, float timeIncrement)
+Vec M3x3vecMultiplication (double matrix[], Vec vector)
 {
-  M2x2 auxMatrix;
-  Vec covector1 = ApplyMetricTensor(vector1);
-  Vec covector2 = ApplyMetricTensor(vector2);
-  auxMatrix.m00 = (vector1.tComponent * covector2.tComponent - vector2.tComponent * covector1.tComponent) + 1;
-  auxMatrix.m01 = (vector1.tComponent * covector2.xComponent - vector2.tComponent * covector1.xComponent) * timeIncrement;
-  auxMatrix.m10 = (vector1.xComponent * covector2.tComponent - vector2.xComponent * covector1.tComponent) * timeIncrement;
-  auxMatrix.m11 = (vector1.xComponent * covector2.xComponent - vector2.xComponent * covector1.xComponent) + 1;
-  return auxMatrix;
+	Vec auxVector1 = {matrix[0], matrix[3], matrix[6]};
+	Vec auxVector2 = {matrix[1], matrix[4], matrix[7]};
+	Vec auxVector3 = {matrix[2], matrix[5], matrix[8]};
+
+	auxVector1 = VecScale(auxVector1, vector.tComponent);
+	auxVector2 = VecScale(auxVector2, vector.xComponent);
+	auxVector3 = VecScale(auxVector3, vector.yComponent);
+
+	return VecSum3(auxVector1, auxVector2, auxVector3);
 }
 
-Vec VecM2x2Multiplication (Vec vector, M2x2 matrix)
+Vec VecSum2(Vec vector1, Vec vector2)
 {
-  Vec auxVector;
-  auxVector.tComponent = vector.tComponent * matrix.m00 + vector.xComponent * matrix.m01;
-  auxVector.xComponent = vector.tComponent * matrix.m10 + vector.xComponent * matrix.m11;
-  return auxVector;
+	Vec auxVector;
+	auxVector.tComponent = vector1.tComponent + vector2.tComponent;
+	auxVector.xComponent = vector1.xComponent + vector2.xComponent;
+	auxVector.yComponent = vector1.yComponent + vector2.yComponent;
+	return auxVector;
+}
+
+Vec VecSum3(Vec vector1, Vec vector2, Vec vector3)
+{
+	Vec auxVector;
+	auxVector.tComponent = vector1.tComponent + vector2.tComponent + vector3.tComponent;
+	auxVector.xComponent = vector1.xComponent + vector2.xComponent + vector3.tComponent;
+	auxVector.yComponent = vector1.yComponent + vector2.yComponent + vector3.tComponent;
+	return auxVector;
 }
 
 Vec VecScale (Vec vector, float factor)
 {
-  Vec auxVector = {vector.tComponent * factor, vector.xComponent * factor};
-  return auxVector;
+	Vec auxVector = {vector.tComponent * factor, vector.xComponent * factor, vector.yComponent * factor};
+	return auxVector;
 }
 
 Vec ApplyMetricTensor (Vec vector)
 {
-  Vec auxVector = {vector.tComponent, - (vector.xComponent)};
-  return auxVector;
+	Vec auxVector = {vector.tComponent, - (vector.xComponent), - (vector.yComponent)};
+	return auxVector;
 }
 
-Vec SetInitialVelocity()
+void ArrayScale (const double *sourceArray, double *array, int d, double factor)
 {
-  float initialSpacialVelocity;
-  float gamma;
-  printf("spacial velocity: ");
-  scanf("%f", &initialSpacialVelocity);
-  gamma = 1 / sqrt(1 - pow(initialSpacialVelocity, 2));
-  Vec fourVelocity = {gamma, gamma * initialSpacialVelocity};
-  return fourVelocity;
-}
-
-Vec SetInitialAcceleration(Vec fourVelocity)
-{
-  float properAcceleration;
-  printf("proper acceleration: ");
-  scanf("%f", &properAcceleration);
-  Vec fourAcceleration = {properAcceleration * fourVelocity.xComponent, properAcceleration * fourVelocity.tComponent};
-  return fourAcceleration;
-}
-
-Vec SetInitialRelativePosition()
-{
-  Vec fourPosition = {0.0, 0.0};
-  printf("spacial position: ");
-  scanf("%f", &fourPosition.xComponent);
-  return fourPosition;
+	for (int i = 0; i < d; i++)
+	{
+		array[i] = sourceArray[i] * factor;
+	}
+	return;
 }
 
 
 int main()
 {
-  if (!SDL_Init(SDL_INIT_VIDEO))
-  {
-    return -1;
-  }
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		return -1;
+	}
 
-  int windowWidth = 2080, windowHeight = 940, windowFlag = SDL_WINDOW_RESIZABLE;
+	int windowWidth = 720, windowHeight = 720, windowFlag = SDL_WINDOW_RESIZABLE;
 
-  Vec velocity = SetInitialVelocity();
-  Vec acceleration = SetInitialAcceleration(velocity);
-  Vec relativePosition = SetInitialRelativePosition();
-  
-  Vec realPosition = {relativePosition.tComponent + windowHeight/2, relativePosition.xComponent + windowWidth/2};
-  
-  float timeIncrement = 0.05;
-  
-  M2x2 lambdaMatrix = AuxM2x2(acceleration, velocity, timeIncrement);
-  printf("%f %f %f %f\n", lambdaMatrix.m00, lambdaMatrix.m01, lambdaMatrix.m10, lambdaMatrix.m11);
 
-  SDL_Window* window = SDL_CreateWindow("simulation", windowWidth, windowHeight, windowFlag);
-  if (window == NULL)
-  {
-    SDL_Quit();
-    return -1;
-  }
+	SDL_Window* window = SDL_CreateWindow("simulation", windowWidth, windowHeight, windowFlag);
+	if (window == NULL)
+	{
+		SDL_Quit();
+		return -1;
+	}
 
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-  
-  if (renderer == NULL)
-  {
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return -1;
-  }
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
 
-  SDL_SetRenderVSync(renderer, 1);
+	if (renderer == NULL)
+	{
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return -1;
+	}
 
-  bool continueLoop = true; 
-  SDL_Event event;
+	//////////////////////////////////////
 
-  float referenceLineX = windowWidth / 2;
-  float referenceLineT = windowHeight / 2;
+	float referencePointX = windowWidth / 2;
+	float referencePointT = windowHeight / 2;
 
-  while (continueLoop)
-  {
-    SDL_PollEvent(&event);
-    if (event.type == SDL_EVENT_QUIT)
-      continueLoop = false;
-    
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderPoint(renderer, realPosition.xComponent, realPosition.tComponent);
+	double constOmega[9] = {
+		0, 0, 0,
+		0, 0, -3,
+		0, 3, 0
+	};
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderPoint(renderer, referenceLineX, referenceLineT);
-    referenceLineX += timeIncrement * 10;
-    referenceLineT -= timeIncrement * 10;
+	double timeScaledOmega[9] = {0};
 
-    SDL_RenderPresent(renderer);
-    velocity = VecM2x2Multiplication(velocity, lambdaMatrix);
-    acceleration = VecM2x2Multiplication(acceleration, lambdaMatrix);
-    lambdaMatrix = AuxM2x2(acceleration, velocity, timeIncrement);
-    realPosition.xComponent = realPosition.xComponent + velocity.xComponent * timeIncrement * 10;
-    realPosition.tComponent = realPosition.tComponent - velocity.tComponent * timeIncrement * 10;
-  }
+	double *omegaExponential;
 
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+	double v = 0.05;
+	double gamma = 1/sqrt(1 - pow(v, 2));
+	Vec velocity = {gamma, gamma * v, 0};
+	Vec realPosition = {0, 0, 0};
 
-  return 0;
+	double elapsedTime = 0.005;
+	double timeStep = 0.005;
+	
+	/////////////////////////////////////
+
+	SDL_SetRenderVSync(renderer, 1);
+
+	bool continueLoop = true; 
+	SDL_Event event;
+
+	while (continueLoop)
+	{
+
+		ArrayScale(constOmega, timeScaledOmega, 9, elapsedTime);
+
+		omegaExponential = r8mat_expm1(3, timeScaledOmega);
+
+		velocity = M3x3vecMultiplication(omegaExponential, velocity);
+
+		realPosition = VecSum2(realPosition, VecScale(velocity, timeStep * 50));
+			
+		free(omegaExponential);
+
+		elapsedTime += timeStep;
+
+		///////////////////////////////////////////////////
+
+		SDL_PollEvent(&event);
+		if (event.type == SDL_EVENT_QUIT)
+			continueLoop = false;
+
+		//renderiza o ponto que estaria em movimento
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderPoint(renderer, referencePointX + realPosition.xComponent , referencePointT - realPosition.tComponent);
+
+		//atualiza a tela
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
